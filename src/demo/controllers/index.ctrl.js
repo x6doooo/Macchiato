66519655134e3f7b8d29c3ab977b6614e0ac9cff
x6doooo <x6doooo@gmail.com>
2015-03-15 09:34:15 +0800

    add demo

--- src/demo/controllers/index.ctrl.js
+++ src/demo/controllers/index.ctrl.js
@@ -1,3 +1,15 @@
 /**
  * Created by dx.yang on 15/3/15.
  */
+
+angular.module('demo')
+    .controller('indexCtrl', [
+        '$scope',
+        'macchiato.ipService',
+        function(
+            $scope,
+            macchiatoIpService
+        ) {
+            $scope.ip = macchiatoIpService.IP2Num('127.0.0.1')
+        }
+    ]);
